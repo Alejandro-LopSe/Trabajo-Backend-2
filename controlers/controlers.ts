d522d9fb836ff2e6dd13e90a1a0ed50150295e0e
alejandrols12 <alopezs5@alumnos.nebrija.es>
2023-11-30 19:47:26 +0100

    V-0.13

--- controlers/controlers.ts
+++ controlers/controlers.ts
@@ -1,10 +1,41 @@
+// deno-lint-ignore-file
 
 import { Studentmodel, Studentmodeltype } from "../mongo/models/student.ts";
 import { Subjectmodel, Subjectmodeltype } from "../mongo/models/subject.ts";
 import { Teachermodel, Teachermodeltype } from "../mongo/models/teacher.ts";
-import { Subject, Teacher,Student } from "../mongo/types.ts";
+import { Subject, Teacher,Student, Errormongo} from "../mongo/types.ts";
 
 
+//Gestion de errores
+export const geterror = (error: Object): Errormongo[]=>{
+
+    const EM: Errormongo = error as Errormongo
+    if(EM.code){
+        if(EM.code===11000){
+            return [{
+                code: EM.code,
+                errorOrigin: "MongoDB",
+                Error: `ya existe este atibuto ${JSON.stringify(EM.keyValue)}`
+            }]
+        }
+    }else if(EM.errors){
+        const errores = Object.keys(EM.errors).map((elem)=>{
+
+            return {
+                code: 1,
+                errorOrigin: "Mongo Error",
+                Error: EM._message,
+                path: EM.errors?.[elem].properties?.path,
+                Cause: EM.errors?.[elem]._message,
+                value: EM.errors?.[elem].properties?.value,
+                type: EM.errors?.[elem].kind
+            }
+        })
+
+        return errores
+    }
+    return [EM]
+}
 
 export const getsubject = async ( elem: Subjectmodeltype): Promise<Subject> => {
 
